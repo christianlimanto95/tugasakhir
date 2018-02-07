f1508b5af53666df17a61cb57bea9e73026f70d1
christianlimanto95 <christianlimanto95@gmail.com>
2018-02-07 11:10:42 +0700

    lil fix

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,6 +2,7 @@ var sheetCanvas, sheetContext, sheetTempCanvas, sheetTempContext;
 var draggableComponent, draggableComponentId = "-1";
 var isDraggingFromToolbar = false;
 var mouseDown = false, mouseDrag = false;
+var dotsHit = "";
 var keyPressed = {
 	"ctrl": false
 };
@@ -32,59 +33,87 @@ $(function() {
 		Sheet.offset = $(".sheet").offset();
 	});
 
-	$(document).on("mousedown", ".component-item", function(e) {
-		var componentId = $(this).data("id");
-		draggableComponentId = componentId;
-		var component = ALL_COMPONENTS.getComponentById(componentId);
-		
-		var width = component.default_size.width;
-		var height = component.default_size.height;
-		var image = component.image;
+	$(document).on("mousedown", ".component-item", e_mousedown_componentItem);
+	$(document).on("click", e_click_document);
+	$(".sheet-temp").on("dblclick", e_dblclick_sheetTemp);
+	$(".component-edit").on("keydown", e_keydown_componentEdit);
+	$(".component-edit-textarea").on("keydown", e_keydown_componentEditTextarea);
+	$(".sheet-temp").on("mousedown", e_mousedown_sheetTemp);
+	$(document).on("mousemove", e_mousemove_document);
+	$(document).on("mouseup", e_mouseup_document);
+	$(document).on("keydown", e_keydown_document);
+	$(document).on("keyup", e_keyup_document);
+});
 
-		draggableComponent.css({
-			"width": width + "px",
-			"height": height + "px",
-			"background-image": "url('" + image + "')",
-			"top": e.pageY + "px",
-			"left": e.pageX + "px"
+function e_mousedown_componentItem(e) {
+	var componentId = $(this).data("id");
+	draggableComponentId = componentId;
+	var component = ALL_COMPONENTS.getComponentById(componentId);
+	
+	var width = component.default_size.width;
+	var height = component.default_size.height;
+	var image = component.image;
+
+	draggableComponent.css({
+		"width": width + "px",
+		"height": height + "px",
+		"background-image": "url('" + image + "')",
+		"top": e.pageY + "px",
+		"left": e.pageX + "px"
+	});
+	draggableComponent.addClass("dragging");
+	isDraggingFromToolbar = true;
+}
+
+function e_click_document(e) {
+	if ($(e.target).closest(".component-edit, .component-edit-textarea").length == 0) {
+		hideComponentEdit();
+	}
+}
+
+function e_dblclick_sheetTemp(e) {
+	var coor = translateMouseCoorToComputedCoor(e);
+	var component = Sheet.isHittingComponent(coor.x, coor.y);
+	if (component != null) {
+		var textCoor = translateComputedCoorBack(component.computed_position.left, component.computed_position.top);
+		$(".component-edit, .component-edit-textarea").removeClass("show");
+		$(".component-edit, .component-edit-textarea").val("");
+		var editText;
+		if (component.multiline) {
+			editText = $(".component-edit-textarea");
+		} else {
+			editText = $(".component-edit");
+		}
+
+		editText.attr("data-temp-id", component.temp_id);
+		editText.css({
+			left: textCoor.x + "px",
+			top: textCoor.y + "px"
 		});
-		draggableComponent.addClass("dragging");
-		isDraggingFromToolbar = true;
-	});
+		editText.val(component.text);
+		editText.addClass("show");
+		editText.focus();
+	}
+}
 
-	$(document).on("click", function(e) {
-		if ($(e.target).closest(".component-edit, .component-edit-textarea").length == 0) {
-			hideComponentEdit();
-		}
-	});
+function e_keydown_componentEdit(e) {
+	if (e.which == 13 || e.which == 27) {
+		e.preventDefault();
+		hideComponentEdit();
+	}
+}
 
-	$(".sheet-temp").on("dblclick", function(e) {
-		var coor = translateMouseCoorToComputedCoor(e);
-		var component = Sheet.isHittingComponent(coor.x, coor.y);
-		if (component != null) {
-			var textCoor = translateComputedCoorBack(component.computed_position.left, component.computed_position.top);
-			$(".component-edit, .component-edit-textarea").removeClass("show");
-			$(".component-edit, .component-edit-textarea").val("");
-			var editText;
-			if (component.multiline) {
-				editText = $(".component-edit-textarea");
-			} else {
-				editText = $(".component-edit");
-			}
+function e_keydown_componentEditTextarea(e) {
+	if (e.which == 27) {
+		e.preventDefault();
+		hideComponentEdit();
+	}
+}
 
-			editText.attr("data-temp-id", component.temp_id);
-			editText.css({
-				left: textCoor.x + "px",
-				top: textCoor.y + "px"
-			});
-			editText.val(component.text);
-			editText.addClass("show");
-			editText.focus();
-		}
-	});
-
-	$(".sheet-temp").on("mousedown", function(e) {
-		var coor = translateMouseCoorToComputedCoor(e);
+function e_mousedown_sheetTemp(e) {
+	var coor = translateMouseCoorToComputedCoor(e);
+	dotsHit = isHittingDots(coor);
+	if (dotsHit == "") {
 		var component = Sheet.isHittingComponent(coor.x, coor.y);
 		if (component != null) {
 			Sheet.isHittingComponentOnMouseDown = true;
@@ -104,77 +133,82 @@ $(function() {
 				SheetTemp.addComponent(Sheet.active_components[i]);
 			}
 		}
+	}
+	
+	Sheet.draw(sheetContext);
+	mouseDown = true;
+}
+
+function e_mousemove_document(e) {
+	if (isDraggingFromToolbar) {
+		draggableComponent.css({
+			"top": e.pageY + "px",
+			"left": e.pageX + "px"
+		});
+	}
+	else if (dotsHit != "") {
 		
-		Sheet.draw(sheetContext);
-		mouseDown = true;
-	});
-
-	$(document).on("mousemove", function(e) {
-		if (isDraggingFromToolbar) {
-			draggableComponent.css({
-				"top": e.pageY + "px",
-				"left": e.pageX + "px"
-			});
-		} else {
-			if (Sheet.isHittingComponentOnMouseDown && Sheet.active_components.length > 0) {
-				SheetTemp.updateComponentsPosition({x: e.pageX, y: e.pageY});
-				SheetTemp.draw(sheetTempContext);
-				mouseDrag = true;
-			} else {
-				mousemoveIsHoveringDots(e);
-			}
-		}
-	});
-
-	$(document).on("mouseup", function(e) {
-		Sheet.isHittingComponentOnMouseDown = false;
-		if (isDraggingFromToolbar) {
-			releaseDragging(e);
-		} else {
-			var coor = translateMouseCoorToComputedCoor(e);
-			var component = Sheet.isHittingComponent(coor.x, coor.y);
-			
-			if (mouseDrag) {
-				Sheet.updateActiveComponentsPosition(SheetTemp.components);
-			} else {
-				if (!keyPressed.ctrl) {
-					Sheet.removeAllActiveComponents();
-					if (component != null) {
-						Sheet.setActiveComponent(component.temp_id);
-					}
-				}
-			}
-
-			SheetTemp.removeAllComponents();
+	} else {
+		if (Sheet.isHittingComponentOnMouseDown && Sheet.active_components.length > 0) {
+			SheetTemp.updateComponentsPosition({x: e.pageX, y: e.pageY});
 			SheetTemp.draw(sheetTempContext);
-			Sheet.draw(sheetContext);
+			mouseDrag = true;
+		} else {
+			mousemoveIsHoveringDots(e);
 		}
-		mouseDown = false;
-		mouseDrag = false;
-	});
+	}
+}
 
-	$(document).on("keydown", function(e) {
-		switch (e.which) {
-			case 46:
-				if (Sheet.active_components.length > 0) {
-					Sheet.deleteComponentsFromActiveComponents();
-					Sheet.draw(sheetContext);
+function e_mouseup_document(e) {
+	Sheet.isHittingComponentOnMouseDown = false;
+	if (isDraggingFromToolbar) {
+		releaseDragging(e);
+	} else {
+		var coor = translateMouseCoorToComputedCoor(e);
+		var component = Sheet.isHittingComponent(coor.x, coor.y);
+		
+		if (mouseDrag) {
+			Sheet.updateActiveComponentsPosition(SheetTemp.components);
+			console.log("A");
+		} else {
+			if (!keyPressed.ctrl) {
+				Sheet.removeAllActiveComponents();
+				if (component != null) {
+					Sheet.setActiveComponent(component.temp_id);
 				}
-				break;
-			case 17:
-				keyPressed.ctrl = true;
-				break;
+			}
 		}
-	});
 
-	$(document).on("keyup", function(e) {
-		switch (e.which) {
-			case 17:
-				keyPressed.ctrl = false;
-				break;
-		}
-	});
-});
+		SheetTemp.removeAllComponents();
+		SheetTemp.draw(sheetTempContext);
+		Sheet.draw(sheetContext);
+	}
+	mouseDown = false;
+	mouseDrag = false;
+	dotsHit = "";
+}
+
+function e_keydown_document(e) {
+	switch (e.which) {
+		case 46:
+			if (Sheet.active_components.length > 0) {
+				Sheet.deleteComponentsFromActiveComponents();
+				Sheet.draw(sheetContext);
+			}
+			break;
+		case 17:
+			keyPressed.ctrl = true;
+			break;
+	}
+}
+
+function e_keyup_document(e) {
+	switch (e.which) {
+		case 17:
+			keyPressed.ctrl = false;
+			break;
+	}
+}
 
 function hideComponentEdit() {
 	var editText = $(".component-edit.show, .component-edit-textarea.show");
@@ -190,8 +224,13 @@ function hideComponentEdit() {
 
 function mousemoveIsHoveringDots(e) {
 	var coor = translateMouseCoorToComputedCoor(e);
+	var found = isHittingDots(coor);
+	$(sheetTempCanvas).attr("cursor-resize", found);
+}
+
+function isHittingDots(coor) {
 	var iLength = Sheet.active_components.length;
-	var found = false;
+	var found = "";
 	for (var i = 0; i < iLength; i++) {
 		var component = Sheet.active_components[i];
 
@@ -203,43 +242,33 @@ function mousemoveIsHoveringDots(e) {
 		var bottom = component.computed_position.bottom;
 
 		if (coor.x >= left - 5 && coor.x <= left + 5 && coor.y >= top - 5 && coor.y <= top + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "top-left");
-			found = true;
+			found = "top-left";
 			break;
 		} else if (coor.x >= center - 5 && coor.x <= center + 5 && coor.y >= top - 5 && coor.y <= top + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "top-center");
-			found = true;
+			found = "top-center";
 			break;
 		} else if (coor.x >= right - 5 && coor.x <= right + 5 && coor.y >= top - 5 && coor.y <= top + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "top-right");
-			found = true;
+			found = "top-right";
 			break;
 		} else if (coor.x >= left - 5 && coor.x <= left + 5 && coor.y >= middle - 5 && coor.y <= middle + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "middle-left");
-			found = true;
+			found = "middle-left";
 			break;
 		} else if (coor.x >= right - 5 && coor.x <= right + 5 && coor.y >= middle - 5 && coor.y <= middle + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "middle-right");
-			found = true;
+			found = "middle-right";
 			break;
 		} else if (coor.x >= left - 5 && coor.x <= left + 5 && coor.y >= bottom - 5 && coor.y <= bottom + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "bottom-left");
-			found = true;
+			found = "bottom-left";
 			break;
 		} else if (coor.x >= center - 5 && coor.x <= center + 5 && coor.y >= bottom - 5 && coor.y <= bottom + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "bottom-center");
-			found = true;
+			found = "bottom-center";
 			break;
 		} else if (coor.x >= right - 5 && coor.x <= right + 5 && coor.y >= bottom - 5 && coor.y <= bottom + 5) {
-			$(sheetTempCanvas).attr("cursor-resize", "bottom-right");
-			found = true;
+			found = "bottom-right";
 			break;
 		}
 	}
 
-	if (!found) {
-		$(sheetTempCanvas).attr("cursor-resize", "");
-	}
+	return found;
 }
 
 function releaseDragging(e) {
